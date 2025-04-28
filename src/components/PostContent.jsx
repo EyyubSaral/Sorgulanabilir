349ffbbc005673cccdec538efc1200da76d69465
Eyyub Saral <eyyubsrl04@gmail.com>
2025-04-28 15:24:20 +0300

    Sorgulanabilir

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,48 +1,70 @@
+import { useEffect, useState } from "react";
+
 export default function PostContent(props) {
   const { image1, image2, mainQuestion, metaData, subQuestions, comments } =
-    props.data
-  const { userName, timeOfPost, filedUnder, profilePic } = metaData
+    props.data;
+  const { userName, timeOfPost, filedUnder, profilePic } = metaData;
+
+  function FormattedDate({ isoString }) {
+    const [formattedDate, setFormattedDate] = useState("");
+
+    useEffect(() => {
+      const date = new Date(isoString);
+      const formatted = date.toLocaleString("tr-TR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      setFormattedDate(formatted);
+    }, [isoString]);
+
+    return <span>{formattedDate}</span>;
+  }
 
   const subQuestionElements = subQuestions.map((question) => (
-    <li key={crypto.randomUUID()} className='sub-question'>
-      <a href={question.link} target='_blank'>
-        {question.text}{' '}
+    <li key={crypto.randomUUID()} className="sub-question">
+      <a href={question.link} target="_blank">
+        {question.text}{" "}
       </a>
     </li>
-  ))
+  ));
 
   return (
     <div>
-      <div className='top-post-overall-container'>
-        <div className='top-post-data-container'>
-          <img className='profile-pic' src={profilePic} />
-          <div className='user-and-category-container'>
-            <p className='post-category'>
-              <a href='#'>{filedUnder}</a>
+      <div className="top-post-overall-container">
+        <div className="top-post-data-container">
+          <img className="profile-pic" src={profilePic} />
+          <div className="user-and-category-container">
+            <p className="post-category">
+              <a href="#">{filedUnder}</a>
             </p>
-            <p className='post-username'>
-              <a href='#'>@{userName}</a>{' '}
+            <p className="post-username">
+              <a href="#">@{userName}</a>{" "}
             </p>
           </div>
         </div>
-        <img className='dots-icon' src='/images/icons/dots.svg' />
+        <img className="dots-icon" src="/images/icons/dots.svg" />
       </div>
-      <h3 className='post-headline'>{mainQuestion}</h3>
-      <div className='post-images-and-sub-questions-container'>
-        <div className='post-images-container'>
-          <img className='post-image' src={image1} />
-          <img className='post-image' src={image2} />
+      <h3 className="post-headline">{mainQuestion}</h3>
+      <div className="post-images-and-sub-questions-container">
+        <div className="post-images-container">
+          <img className="post-image" src={image1} />
+          <img className="post-image" src={image2} />
         </div>
-        <div className='sub-questions-container'>
+        <div className="sub-questions-container">
           <ul>{subQuestionElements}</ul>
         </div>
       </div>
-      <div className='bottom-post-data-container'>
-        <p>{timeOfPost}</p>
+      <div className="bottom-post-data-container">
+        <p>
+          <FormattedDate isoString={timeOfPost} />
+        </p>
         <p>
           <a>{comments.length} yorum</a>
         </p>
       </div>
     </div>
-  )
+  );
 }
